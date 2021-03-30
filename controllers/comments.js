c4d6cb80ed5858bd542648aeab5fc1f5a6aefd0b
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-30 17:45:45 -0400

    comments can be deleted

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,7 +6,19 @@ module.exports = {
 }
 
 function deleteOne(req, res) {
-
+  Playlist.findOne(
+    {'comments._id': req.params.id, 'comments.userId': req.user._id},
+    function(err, playlist) {
+      if (!playlist || err) return res.redirect(`/playlists/${playlist._id}`);
+      // Remove the subdoc (https://mongoosejs.com/docs/subdocs.html)
+      playlist.comments.remove(req.params.id);
+      // Save the updated playlist
+      playlist.save(function(err) {
+        // Redirect back to the playlist's show view
+        res.redirect(`/playlists/${playlist._id}`);
+      });
+    }
+  );
 }
 
 function create(req, res) {

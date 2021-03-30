7d635ac95399fc2e36d9c62dbfc54fc4f7774f6f
Jack Huntington-Rainey <jack@jacks-mbp-2.myfiosgateway.com>
2021-03-30 18:53:58 -0400

    users can update their own comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,6 +3,25 @@ const Playlist = require('../models/playlist');
 module.exports = {
   create,
   delete: deleteOne,
+  update,
+}
+
+function update(req, res) {
+  // Note the cool "dot" syntax to query on the property of a subdoc
+  Playlist.findOne({'comments._id': req.params.id}, function(err, playlist) {
+    // Find the comment subdoc using the id method on Mongoose arrays
+    // https://mongoosejs.com/docs/subdocs.html
+    const commentSubdoc = playlist.comments.id(req.params.id);
+    // Ensure that the comment was created by the logged in user
+    if (!commentSubdoc.userId.equals(req.user._id)) return res.redirect(`/playlists/${playlist._id}`);
+    // Update the text of the comment
+    commentSubdoc.content = req.body.content;
+    // Save the updated playlist
+    playlist.save(function(err) {
+      // Redirect back to the playlist's show view
+      res.redirect(`/playlists/${playlist._id}`);
+    });
+  });
 }
 
 function deleteOne(req, res) {
@@ -10,11 +29,8 @@ function deleteOne(req, res) {
     {'comments._id': req.params.id, 'comments.userId': req.user._id},
     function(err, playlist) {
       if (!playlist || err) return res.redirect(`/playlists/${playlist._id}`);
-      // Remove the subdoc (https://mongoosejs.com/docs/subdocs.html)
       playlist.comments.remove(req.params.id);
-      // Save the updated playlist
       playlist.save(function(err) {
-        // Redirect back to the playlist's show view
         res.redirect(`/playlists/${playlist._id}`);
       });
     }
--- routes/comments.js
+++ routes/comments.js
@@ -7,4 +7,6 @@ router.post('/playlists/:id/comments', commentsCtrl.create);
 
 router.delete('/comments/:id', commentsCtrl.delete);
 
+router.put('/comments/:id', commentsCtrl.update);
+
 module.exports = router;
